90dd203d8cc32662740cb24e08d9672751c49d8a
The-New-Order <theneworder.git@gmail.com>
2019-04-28 18:56:26 -0700

    MAX_WEAPONS Increased to 64 (SP)

--- code/ui/ui_main.cpp
+++ code/ui/ui_main.cpp
@@ -5559,7 +5559,10 @@ static void UI_ClearWeapons ( void )
 		playerState_t*		pState = cl->gentity->client;
 
 		// Clear out any weapons for the player
-		pState->stats[ STAT_WEAPONS ] = 0;
+		for ( int i = 0; i < MAX_WEAPONS; i++ )
+		{
+			pState->weapons[i] = 0;
+		}
 
 		pState->weapon = WP_NONE;
 
@@ -5583,7 +5586,7 @@ static void UI_GiveWeapon ( const int weaponIndex )
 
 		if (weaponIndex<WP_NUM_WEAPONS)
 		{
-			pState->stats[ STAT_WEAPONS ] |= ( 1 << weaponIndex );
+			pState->weapons[weaponIndex] = 1;
 		}
 	}
 }
@@ -5745,7 +5748,7 @@ static void	UI_AddWeaponSelection ( const int weaponIndex, const int ammoIndex, 
 
 			if ((weaponIndex>0) && (weaponIndex<WP_NUM_WEAPONS))
 			{
-				pState->stats[ STAT_WEAPONS ] |= ( 1 << weaponIndex );
+				pState->weapons[weaponIndex] = 1;
 			}
 
 			// Give them ammo too
@@ -5850,7 +5853,7 @@ static void UI_RemoveWeaponSelection ( const int weaponSelectionIndex )
 
 			if ((weaponIndex>0) && (weaponIndex<WP_NUM_WEAPONS))
 			{
-				pState->stats[ STAT_WEAPONS ]  &= ~( 1 << weaponIndex );
+				pState->weapons[weaponIndex] = 0;
 			}
 
 			// Remove ammo too
@@ -6036,7 +6039,7 @@ static void	UI_AddThrowWeaponSelection ( const int weaponIndex, const int ammoIn
 
 			if ((weaponIndex>0) && (weaponIndex<WP_NUM_WEAPONS))
 			{
-				pState->stats[ STAT_WEAPONS ] |= ( 1 << weaponIndex );
+				pState->weapons[weaponIndex] = 1;
 			}
 
 			// Give them ammo too
@@ -6115,7 +6118,7 @@ static void UI_RemoveThrowWeaponSelection ( void )
 
 			if ((uiInfo.selectedThrowWeapon>0) && (uiInfo.selectedThrowWeapon<WP_NUM_WEAPONS))
 			{
-				pState->stats[ STAT_WEAPONS ]  &= ~( 1 << uiInfo.selectedThrowWeapon );
+				pState->weapons[uiInfo.selectedThrowWeapon] = 0;
 			}
 
 			// Remove ammo too
