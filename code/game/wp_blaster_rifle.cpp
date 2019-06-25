6df4efdc3cf4def8550ff591626dc899b94d3ed5
The-New-Order <theneworder.git@gmail.com>
2019-06-25 11:22:14 -0700

    EE-3 Carbine Rifle (SP)

--- code/game/wp_blaster_rifle.cpp
+++ code/game/wp_blaster_rifle.cpp
@@ -174,3 +174,150 @@ void WP_FireBlaster( gentity_t *ent, qboolean alt_fire )
 		WP_FireBlasterMissile( ent, muzzle, dir, alt_fire );
 	}
 }
+
+//---------------
+//	EE-3 Carbine Rifle
+//---------------
+
+//---------------------------------------------------------
+void WP_FireEE3CarbineRifleMissile(gentity_t *ent, vec3_t start, vec3_t dir, qboolean altFire)
+//---------------------------------------------------------
+{
+	int velocity = BLASTER_VELOCITY;
+	int	damage = altFire ? weaponData[WP_EE3_CARBINE_RIFLE].altDamage : weaponData[WP_EE3_CARBINE_RIFLE].damage;
+
+	if (ent && ent->client && ent->client->NPC_class == CLASS_VEHICLE)
+	{
+		damage *= 3;
+		velocity = ATST_MAIN_VEL + ent->client->ps.speed;
+	}
+	else
+	{
+		// If an enemy is shooting at us, lower the velocity so you have a chance to evade
+		if (ent->client && ent->client->ps.clientNum != 0 && ent->client->NPC_class != CLASS_BOBAFETT)
+		{
+			if (g_spskill->integer < 2)
+			{
+				velocity *= BLASTER_NPC_VEL_CUT;
+			}
+			else
+			{
+				velocity *= BLASTER_NPC_HARD_VEL_CUT;
+			}
+		}
+	}
+
+	WP_TraceSetStart(ent, start, vec3_origin, vec3_origin);//make sure our start point isn't on the other side of a wall
+
+	WP_MissileTargetHint(ent, start, dir);
+
+	gentity_t *missile = CreateMissile(start, dir, velocity, 10000, ent, altFire);
+
+	missile->classname = "blaster_proj";
+	missile->s.weapon = WP_EE3_CARBINE_RIFLE;
+
+	// Do the damages
+	if (ent->s.number != 0 && ent->client->NPC_class != CLASS_BOBAFETT)
+	{
+		if (g_spskill->integer == 0)
+		{
+			damage = BLASTER_NPC_DAMAGE_EASY;
+		}
+		else if (g_spskill->integer == 1)
+		{
+			damage = BLASTER_NPC_DAMAGE_NORMAL;
+		}
+		else
+		{
+			damage = BLASTER_NPC_DAMAGE_HARD;
+		}
+	}
+
+	// if ( ent->client )
+	// {
+	// 	if ( ent->client->ps.powerups[PW_WEAPON_OVERCHARGE] > 0 && ent->client->ps.powerups[PW_WEAPON_OVERCHARGE] > cg.time )
+	// 	{
+	// 		// in overcharge mode, so doing double damage
+	// 		missile->flags |= FL_OVERCHARGED;
+	// 		damage *= 2;
+	// 	}
+	// }
+
+	missile->damage = damage;
+	missile->dflags = DAMAGE_DEATH_KNOCKBACK;
+	if (altFire)
+	{
+		missile->methodOfDeath = MOD_BLASTER_ALT;
+	}
+	else
+	{
+		missile->methodOfDeath = MOD_BLASTER;
+	}
+	missile->clipmask = MASK_SHOT | CONTENTS_LIGHTSABER;
+
+	// we don't want it to bounce forever
+	missile->bounceCount = 8;
+}
+
+//---------------------------------------------------------
+void WP_FireEE3CarbineRifle(gentity_t *ent, qboolean alt_fire)
+//---------------------------------------------------------
+{
+	vec3_t	dir, angs, angles;
+
+	vectoangles(forwardVec, angs);
+
+	if (ent->client && ent->client->NPC_class == CLASS_VEHICLE)
+	{//no inherent aim screw up
+	}
+	else if ( alt_fire && cg.zoomMode >= SCOPE_A280 )
+	{
+		AngleVectors( ent->client->renderInfo.eyeAngles, forwardVec, NULL, NULL );
+		vectoangles( forwardVec, angles );
+
+		angles[PITCH] += Q_flrand(-1.0f, 1.0f) * SCOPE_SPREAD;
+		angles[YAW]   += Q_flrand(-1.0f, 1.0f) * SCOPE_SPREAD;
+	}
+	else if (!(ent->client->ps.forcePowersActive&(1 << FP_SEE))
+		|| ent->client->ps.forcePowerLevel[FP_SEE] < FORCE_LEVEL_2)
+	{//force sight 2+ gives perfect aim
+		//FIXME: maybe force sight level 3 autoaims some?
+		if (alt_fire)
+		{
+			// add some slop to the alt-fire direction
+			angs[PITCH] += Q_flrand(-1.0f, 1.0f) * BLASTER_ALT_SPREAD;
+			angs[YAW] += Q_flrand(-1.0f, 1.0f) * BLASTER_ALT_SPREAD;
+		}
+		else
+		{
+			// Troopers use their aim values as well as the gun's inherent inaccuracy
+			// so check for all classes of stormtroopers and anyone else that has aim error
+			if (ent->client && ent->NPC &&
+				(ent->client->NPC_class == CLASS_STORMTROOPER ||
+				ent->client->NPC_class == CLASS_SWAMPTROOPER))
+			{
+				angs[PITCH] += (Q_flrand(-1.0f, 1.0f) * (BLASTER_NPC_SPREAD + (6 - ent->NPC->currentAim)*0.25f));//was 0.5f
+				angs[YAW] += (Q_flrand(-1.0f, 1.0f) * (BLASTER_NPC_SPREAD + (6 - ent->NPC->currentAim)*0.25f));//was 0.5f
+			}
+			else
+			{
+				// add some slop to the main-fire direction
+				angs[PITCH] += Q_flrand(-1.0f, 1.0f) * BLASTER_MAIN_SPREAD;
+				angs[YAW] += Q_flrand(-1.0f, 1.0f) * BLASTER_MAIN_SPREAD;
+			}
+		}
+	}
+
+	if ( alt_fire && cg.zoomMode >= SCOPE_A280 )
+	{
+		AngleVectors( angles, forwardVec, NULL, NULL );
+		WP_FireBlasterMissile( ent, ent->client->renderInfo.eyePoint, forwardVec, alt_fire );
+	}
+	else
+	{
+		AngleVectors( angs, dir, NULL, NULL );
+
+		// FIXME: if temp_org does not have clear trace to inside the bbox, don't shoot!
+		WP_FireBlasterMissile( ent, muzzle, dir, alt_fire );
+	}
+}
